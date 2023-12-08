9a35a658ad52d06b9ce0fc790ab47e7cda2a80f5
nemuo1208 <151502454+nemuo1208@users.noreply.github.com>
2023-12-08 10:36:02 +0900

    エラー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,20 @@
 class UsersController < ApplicationController
- 
+  def create
+    User.create(protospace_params)
+    binding.pry
+    redirect_to '/'
+  end
+
+
+
+  private
+   def protospace_params
+     params.require(:user).permit(:eamail, :password, :nickname, :profile, :affiliction, :role)
+   end
+
+
+
+
+
+
 end
