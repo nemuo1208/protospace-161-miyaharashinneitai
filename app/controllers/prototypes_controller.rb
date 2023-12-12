e5dbd4551a187ae7a2515dfe3a1c18a804b6a154
nemuo1208 <151502454+nemuo1208@users.noreply.github.com>
2023-12-12 16:07:07 +0900

    事故

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,6 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
       @prototypes = Prototype.all
   end  
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,10 @@ class UsersController < ApplicationController
   end
 
 
+  def show
+    @user = User.find(params[:id])
+    @prototypes = @user.prototypes
+  end  
 
   private
    def protospace_params
